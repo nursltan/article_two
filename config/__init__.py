f6cb7af4e85ae910f241ac723c9d88b58f207f66
NurikQA <@nursltan>
2023-10-17 15:36:18 +0600

    Added modules to config

---
+++ config/__init__.py
@@ -0,0 +1,7 @@
+from config.Url import Url
+from config.expectations import Expectations
+from config.playwright import Playwright
+
+url = Url()
+expectations = Expectations()
+playwright = Playwright()
